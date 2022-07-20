b410dafe1ae28635b4c56189bb5a30167acebd35
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-07-20 17:59:50 -0300

    Adding header

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import "normalize.css";
+import "./index.css";
 import Cardapio from "./pages/Cardapio/Index";
 
 const root = ReactDOM.createRoot(
