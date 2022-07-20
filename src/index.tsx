b631739ac18e222676427ad7e45db7fe241136c3
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-07-20 17:03:09 -0300

    Adding reset css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./reset.css";
 import Cardapio from "./pages/Cardapio/Index";
 
 const root = ReactDOM.createRoot(
