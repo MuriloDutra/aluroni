4e109a9ef03938068df310d4436608cd1a0d459f
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-07-31 20:11:10 -0300

    User can filter dishes by name and category

--- src/pages/Cardapio/Itens/Index.tsx
+++ src/pages/Cardapio/Itens/Index.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
 import Item from "./item/Index";
 import cardapio from "./Itens.json";
 import styles from "./Itens.module.scss";
 
-export default function Itens() {
+interface Props {
+  busca: string;
+  filtro: number | null;
+  ordenador: string;
+}
+
+export default function Itens({ busca, filtro, ordenador }: Props) {
+  const [lista, setLista] = useState(cardapio);
+
+  useEffect(() => {
+    const novaLista = cardapio.filter(
+      (item) => testaBusca(item.title) && testaFiltro(item.category.id)
+    );
+    setLista(novaLista);
+  }, [busca, filtro]);
+
+  function testaBusca(title: string) {
+    const regex = new RegExp(busca, "i");
+    return regex.test(title);
+  }
+
+  function testaFiltro(id: number) {
+    if (filtro !== null) return filtro === id;
+    return true;
+  }
+
   return (
     <div className={styles.itens}>
-      {cardapio.map((item) => (
+      {lista.map((item) => (
         <Item key={item.id} {...item} />
       ))}
     </div>
